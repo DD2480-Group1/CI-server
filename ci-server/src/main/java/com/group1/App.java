286b35ae897877330a1d2cbc71bc6312aff1d5f6
ZOU Hetai <33616271+JXNCTED@users.noreply.github.com>
2024-02-06 14:06:48 +0100

    update pom.xml and pass compile and start a server
    Working on #14

--- ci-server/src/main/java/com/group1/App.java
+++ ci-server/src/main/java/com/group1/App.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Handler.Abstract;;
+import org.eclipse.jetty.server.handler.AbstractHandler;
 
 public class App extends AbstractHandler {
     public void handle(String target,
@@ -16,28 +16,28 @@ public class App extends AbstractHandler {
             HttpServletRequest request,
             HttpServletResponse response)
             throws IOException, ServletException {
-        // response.setContentType("text/html;charset=utf-8");
-        // response.setStatus(HttpServletResponse.SC_OK);
-        // baseRequest.setHandled(true);
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
 
-        // System.out.println(target);
+        System.out.println(target);
 
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
         // 2nd compile the code
 
-        // response.getWriter().println("CI job done");
+        response.getWriter().println("CI job done");
     }
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
-    // used to start the CI server in command line
-    // public static void main(String[] args) throws Exception {
-    // Server server = new Server(8080);
-    // server.setHandler(new ContinuousIntegrationServer());
-    // server.start();
-    // server.join();
+    // public static void main(String[] args) {
+    // System.out.println("Hello, World");
     // }
+    // used to start the CI server in command line
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        server.setHandler(new App());
+        server.start();
+        server.join();
+    }
 }
