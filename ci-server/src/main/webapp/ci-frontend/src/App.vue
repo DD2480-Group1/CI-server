e4f30067e62ece3f212e53f108667751ccf6e1da
ZOU Hetai <33616271+JXNCTED@users.noreply.github.com>
2024-02-10 21:36:10 +0100

    Add change icon color and log text
    Set everything to globla variables and changing the color
    of the icon and the text according to the result
    
    Working on #11

--- ci-server/src/main/webapp/ci-frontend/src/App.vue
+++ ci-server/src/main/webapp/ci-frontend/src/App.vue
@@ -1,5 +1,3 @@
-<script>
-</script>
 <template>
   <header>
     <v-toolbar color="primary" dark>
@@ -27,12 +25,8 @@
     <v-container>
       <v-row>
         <div class="d-flex flex-wrap ga-3">
-          <v-btn color="primary" onclick="console.log('refreshing...')"
-            >Refresh</v-btn
-          >
-          <v-btn color="primary" onclick="console.log('fetching...')"
-            >Fetch</v-btn
-          >
+          <v-btn color="primary" @click="refresh">Refresh</v-btn>
+          <v-btn color="primary" @click="fetch">Fetch</v-btn>
         </div>
       </v-row>
     </v-container>
@@ -43,17 +37,42 @@
       <v-col cols="12">
         <div class="d-flex flex-wrap ga-2">
           <v-chip
-            prepend-icon="mdi-checkbox-marked-circle"
-            variant="outlined"
-            color="green"
+            :prepend-icon="
+              compilePassed == 0
+                ? 'mdi-alert'
+                : compilePassed == 1
+                ? 'mdi-checkbox-marked-circle'
+                : 'mdi-close-circle'
+            "
+            :color="
+              compilePassed == 0 ? 'grey' : compilePassed == 1 ? 'green' : 'red'
+            "
           >
             Compile
           </v-chip>
-          <v-chip prepend-icon="mdi-cancel" variant="outlined" color="red">
+          <v-chip
+            :prepend-icon="
+              testPassed == 0
+                ? 'mdi-alert'
+                : testPassed == 1
+                ? 'mdi-checkbox-marked-circle'
+                : 'mdi-close-circle'
+            "
+            :color="
+              testPassed == 0 ? 'grey' : testPassed == 1 ? 'green' : 'red'
+            "
+          >
             Test
           </v-chip>
         </div>
       </v-col>
+      <v-textarea
+        :readonly="isReadOnly"
+        label="Log"
+        outlined
+        rows="10"
+        v-model="log"
+      ></v-textarea>
     </v-row>
   </v-container>
 </template>
@@ -61,13 +80,30 @@
 export default {
   data: () => ({
     commits: [
-      { title: "commit 1" },
-      { title: "commit 2" },
-      { title: "commit 3" },
-      { title: "commit 4" },
+      // "commit1",
     ],
-    branches: ["main", "develop", "assessment", "feature"],
-    repositories: ["ci-frontend", "ci-backend", "ci-server", "ci-database"],
+    branches: [
+      //  "bugfix",
+    ],
+    repositories: [
+      // "repo1",
+    ],
+    isReadOnly: true,
+    log: "",
+    compilePassed: 0, // 0: not tested, 1: passed, 2: failed
+    testPassed: 0, // 0: not tested, 1: passed, 2: failed
   }),
+
+  methods: {
+    refresh() {
+      console.log("refreshing...");
+      // this.compilePassed = (this.compilePassed + 1) % 3;
+    },
+
+    fetch() {
+      console.log("fetching...");
+      // this.testPassed = (this.testPassed + 1) % 3;
+    },
+  },
 };
 </script>
