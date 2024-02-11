3932af048b84b97c953f0ef3ebe1293132c0234d
Hannes Stig <hannes.a.stig@gmail.com>
2024-02-11 16:37:30 +0100

    Add basic structure to handle Webhook
    Added a basic method that can be used to handle the
    "POST /" request that is sent by GitHub when the webhook is
    triggered.
    
    The methods to compile and and test the project should be ran in this
    function.
    
    Working #22

--- ci-server/src/main/java/com/group1/App.java
+++ ci-server/src/main/java/com/group1/App.java
@@ -13,6 +13,8 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.io.BufferedReader;
+
 public class App extends AbstractHandler {
     public void handle(String target,
             Request baseRequest,
@@ -25,6 +27,11 @@ public class App extends AbstractHandler {
 
         System.out.println(target);
 
+        // Handle POST requests from GitHub
+        if (target.equals("/") && baseRequest.getMethod().equals("POST")) {
+            handleWebhook(target, baseRequest, request, response);
+        }
+
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
@@ -32,22 +39,54 @@ public class App extends AbstractHandler {
 
         response.getWriter().println("CI job done");
     }
-    
+
     /**
      * @param str The string that should be parsed into a JSONObject
-     * @return A JSONObject representing the parsed string, or an empty JSONObject 
-     * if the parsing fails
-     * */
+     * @return A JSONObject representing the parsed string, or an empty JSONObject
+     *         if the parsing fails
+     */
     private JSONObject parseJSON(String str) {
         try {
             JSONParser parser = new JSONParser();
             JSONObject json = (JSONObject) parser.parse(str);
             return json;
         } catch (org.json.simple.parser.ParseException e) {
-            System.err.println("[ERROR] In parseJSON(String): Failed parsing Json from string"); 
+            System.err.println("[ERROR] In parseJSON(String): Failed parsing Json from string");
             return new JSONObject();
         }
     }
+
+    /**
+     * Handles the webhook request from Github
+     */
+    public void handleWebhook(String target,
+            Request baseRequest,
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+        
+        // Parse the JSON file in the request body
+        BufferedReader contentReader = new BufferedReader(baseRequest.getReader());
+        String line = contentReader.readLine();
+        StringBuilder body = new StringBuilder();
+        while (line != null) {
+            body.append(line);
+            line = contentReader.readLine();
+        }
+
+        JSONObject payload = parseJSON(body.toString());
+        
+        // Do the webhook tasks here
+        // Clone repo
+        // checkout branch
+        // build project
+        // test project
+        // write files 
+        // delete cloned repo
+        // respond to github
+    }
+
+
 
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception {
@@ -57,4 +96,3 @@ public class App extends AbstractHandler {
         server.join();
     }
 }
-
