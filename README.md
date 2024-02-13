7d342aa3db573f28683da9ad7b3331f07edd3b3f
ZOU Hetai <33616271+JXNCTED@users.noreply.github.com>
2024-02-13 23:47:35 +0100

    Update README and documentation (#43)
    Update documentation with:
    - additional API documentation
    - documentation generation instruction 
    - Build, test, and deploy instructions
    
    README also include placeholder for statement of contributions and implementation details
    
    Working #8 #2

--- README.md
+++ README.md
@@ -1,29 +1,61 @@
 # assignment-2
 
+## Description
+
+This is a continuous integration server that is designed to be used with a GitHub repository. The server will be able to receive webhooks from GitHub and run tests on the repository. The server will also be able to send notifications to the repository owner about the status of the tests.
+
+A frontend is implemented to allow the user to view the status of the tests and the logs of the tests. The frontend is implemented using vue and the backend is implemented using jetty server.
+
 ## Prerequisites
-* Java version 11 or higher 
+
+* Java version 11 or higher
 * Maven
+* Ngrok (optional)
 
 ## How To Build
+
 ```
-mvn package
+bash build.sh
 ```
 
-## How to Run and Test Build
+## How to Run
+
 ```
-mvn exec:java -D exec.mainClass=com.group1.App
+bash launch.sh
 ```
 
-## How Run Server
+## Use ngrok to expose the server to the internet
+
 ```
-TODO: add description
+ngrok http --domain=<your-ngrok.domain> 8080
 ```
 
-## Commit Structure for assignment-2 
+## How to Test
+
+```bash
+cd ci-server
+mvn test
+```
+
+## Generate Documentation
+
+```bash
+cd ci-server
+mvn javadoc:javadoc
+```
+
+The documentation will be generated in the `target/site/apidocs` directory and the dependencies in the `target/site` directory.
+
+## Implementation Details
+
+### Notification
+
+## Commit Structure for assignment-2
 
 Each commit message should consist of a subject and a body. Please follow this message structure when committing to the project:
 
 **Subject**
+
 * The first line (the title) is the subject and should contain imperative language, present tense: "Fix bug" not "Fixed bug".
 * The subject should also start with a capital letter, not end on a period and be less than fifty characters.
 * Prefix is not required as long as the subject summarizes the change itself.
@@ -31,6 +63,7 @@ Each commit message should consist of a subject and a body. Please follow this m
 After the subject, a blank line should be made followed by the body.
 
 **Body**
+
 * The body should also use imperative language: "Fix missing" instead of "Fixed missing" or "Fixes missing".
 * When possible, use punctuation and capital letters where appropriate.
 * Lines should not exceed 72 characters, except when including compiler error messages.
@@ -40,6 +73,7 @@ After the subject, a blank line should be made followed by the body.
 * Use "Fixes" for bugs and "Closes" for general issues.
 
 Here is an example of a good commit:
+
 ```
 Add missing import of 'gtest' in src/decide.cpp
 
@@ -51,3 +85,15 @@ Fix introduced by adding 'gtest' module to CMakeList.txt.
 Fixes #1002
 Closes #64
 ```
+
+## Statement of Contributions
+
+Ahmed:
+
+ZOU Hetai (Ed):
+
+Elliot:
+
+Yening:
+
+## Essence
--- ci-server/src/main/java/com/group1/App.java
+++ ci-server/src/main/java/com/group1/App.java
@@ -42,9 +42,8 @@ import org.json.simple.JSONArray;
 
 public class App extends AbstractHandler {
     private static final int PORT = 8080;
-    private static final String CI_FRONTEND_URL = "https://skylark-fresh-whale.ngrok-free.app"; //"https://www.google.se/?hl=sv";
+    private static final String CI_FRONTEND_URL = "https://skylark-fresh-whale.ngrok-free.app"; // "https://www.google.se/?hl=sv";
 
-    // TODO: add function description
     public void handle(String target,
             Request baseRequest,
             HttpServletRequest request,
@@ -301,7 +300,7 @@ public class App extends AbstractHandler {
             createCommitStatus(repositoryFullName, commitSHA, "error", token, "compile failed");
             // set compile state to false if compile failed
             compileState = false;
-        } 
+        }
         // if this error is reached, we failed to create a commit status, so do not try
         // to do it again
         catch (Exception e) {
@@ -353,7 +352,15 @@ public class App extends AbstractHandler {
                 .call();
     }
 
-    // TODO: add documentation
+    /**
+     * Function to run shell commands in Java. Uses ProcessBuilder to execute shell
+     * 
+     * @param command the command to be executed
+     * @param file    the working directory
+     * @return
+     * @throws IOException
+     * @throws InterruptedException
+     */
     public static String runCommand(String[] command, File file)
             throws IOException, InterruptedException {
 
@@ -408,7 +415,14 @@ public class App extends AbstractHandler {
         }
     }
 
-    // TODO: add documentation
+    /**
+     * Run tests function uses ProcessBuilder to execture shell commands in Java.
+     * 
+     * @param repoDir location of the repository directory locally on the server
+     * @return the output from the test
+     * @throws IOException
+     * @throws InterruptedException
+     */
     public static String runTests(File repoDir)
             throws IOException, InterruptedException {
 
@@ -525,7 +539,7 @@ public class App extends AbstractHandler {
      *         if no match is found or an error occurs during parsing.
      */
     public int getTestFailures(String testOutput) {
-        
+
         String regex = "Tests run: \\d+, Failures: (\\d+), Errors: (\\d+), Skipped: \\d+";
 
         Pattern pattern = Pattern.compile(regex);
@@ -533,15 +547,20 @@ public class App extends AbstractHandler {
         int failures = -1;
         while (matcher.find()) {
             try {
-              failures= Integer.parseInt(matcher.group(1))+Integer.parseInt(matcher.group(2));
+                failures = Integer.parseInt(matcher.group(1)) + Integer.parseInt(matcher.group(2));
             } catch (NumberFormatException e) {
                 // do nothing
             }
-         }
+        }
         return failures;
     }
 
-    // TODO: add documentation
+    /**
+     * Function to read the request body from the POST request from GitHub
+     * 
+     * @param baseRequest the request object
+     * @return the request body as a StringBuilder
+     */
     private StringBuilder readRequest(Request baseRequest) {
         try {
             BufferedReader contentReader = new BufferedReader(baseRequest.getReader());
@@ -658,6 +677,7 @@ public class App extends AbstractHandler {
     static class CompileException extends Exception {
         /**
          * Constructor to create compile exception with custom message
+         * 
          * @param message Message to be displayed
          */
         public CompileException(String message) {
@@ -665,9 +685,11 @@ public class App extends AbstractHandler {
         }
 
         /**
-         * Constructor to create compile exception with custom message and throwable cause
+         * Constructor to create compile exception with custom message and throwable
+         * cause
+         * 
          * @param message Message to be displayed
-         * @param cause Represents underlying reason why error was thrown
+         * @param cause   Represents underlying reason why error was thrown
          */
         public CompileException(String message, Throwable cause) {
             super(message, cause);
