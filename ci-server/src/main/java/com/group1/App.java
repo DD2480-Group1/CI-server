3bdeabbc8b83f74ef306490df1f5faf022a6385a
Elliot <18326767+real-darth@users.noreply.github.com>
2024-02-13 19:18:30 +0100

    Add data save system to server (#31)
    Add function to save data from CI server console output as well as additional information regarding the commits. This data is stored in a JSON-file located in a file structure adapted to @JXNCTED specifications. JSON commits are named by the order they were committed on the branch and their SHA.
    
    Merge working `branch get-test-fails` into this branch in order to fix server detection of unit test fails. This information was necessary for the save system as well, since the website will display if the commit passes all CI server tests. Now the server can detect if any of the unit tests have failed and report this back to Git via the Git API. 
    
    Add Quality life features:
    - Add documentation to some functions that were missing it.
    - Website URL for frontend is now stored in a config file located under webapp. Tell me if this file should be moved 
    to another part of the repository. Frontend now reads website URL from this file, now we can change one line instead
    of multiple lines.
    
    Co-authored-by: Elliot <edarth@kth.se>
    Co-authored-by: Hannes Stig <hannes.a.stig@gmail.com>
    
    Working #6 #8 #26 
    Closes #25 #30

--- ci-server/src/main/java/com/group1/App.java
+++ ci-server/src/main/java/com/group1/App.java
@@ -3,6 +3,7 @@ package com.group1;
 // read file
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,6 +14,8 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // server 
 import javax.servlet.ServletException;
@@ -26,19 +29,19 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
-
 // GIT
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-
+import org.json.simple.JSONArray;
 // JSON
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.JSONArray;
 
 public class App extends AbstractHandler {
-    private static int PORT = 8080;
+    private static final int PORT = 8080;
+    // TODO: link to real website
+    private static final String CI_FRONTEND_URL = "https://www.google.se/?hl=sv";
 
     // TODO: add function description
     public void handle(String target,
@@ -49,6 +52,9 @@ public class App extends AbstractHandler {
         // debug print
         System.out.println("Handling request: " + target);
         System.out.println("Method: " + baseRequest.getMethod());
+
+        // acknowledge webhook received with 202 status code
+        response.setStatus(HttpServletResponse.SC_ACCEPTED);
 
         if (target.startsWith("/api/repo")) {
             System.out.println("handling /api/repo");
@@ -199,11 +205,24 @@ public class App extends AbstractHandler {
         // get repository URL
         String repositoryURL = (String) repositoryJSON.get("clone_url");
         // get repository name
-        String repositoryName = (String) repositoryJSON.get("full_name");
+        String repositoryFullName = (String) repositoryJSON.get("full_name");
         // get the branch name the commit is on
         String branch = (String) payload.get("ref");
         // get the commit SHA
         String commitSHA = (String) payload.get("after");
+
+        // get extra commit information for CI server history
+        String repositoryName = (String) repositoryJSON.get("name");
+
+        JSONObject headCommitJSON = (JSONObject) payload.get("head_commit");
+        String commitName = (String) headCommitJSON.get("message");
+        // check if commit message is more than one line, handle it correclty
+        if (commitName.indexOf('\n') != -1) {
+            commitName = commitName.substring(0, commitName.indexOf('\n'));
+        }
+        // timestamp
+        String commitTimestamp = (String) headCommitJSON.get("timestamp");
+        String commitURL = (String) headCommitJSON.get("url");
 
         // get github token from secret file
         String token = readToken("secret/github_token.txt");
@@ -211,6 +230,9 @@ public class App extends AbstractHandler {
         // variables we want to store
         String compileOutput = "";
         String testOutput = "";
+        // checks
+        boolean compileState = false;
+        boolean testState = false;
 
         // here you do all the continuous integration tasks
         try {
@@ -219,33 +241,51 @@ public class App extends AbstractHandler {
             Git git = cloneRepository(repositoryURL, branch, token);
             System.out.println("CLONE SUCCESS, starting build, this may take a while...");
             // notify GitHub 
-            createCommitStatus(repositoryName, commitSHA, "pending", token);
+            createCommitStatus(repositoryFullName, commitSHA, "pending", token, "compiling repo on server...");
             
             // 2nd compile the code
             File repoDirectory = git.getRepository().getDirectory().getParentFile();
             compileOutput = compileRepository(repoDirectory);
             System.out.println("COMPILE SUCCESS, compiling clone was successfull!");
+            // set compile state, compilation was successfull
+            compileState = true;
+
             // 3rd run tests
             testOutput = runTests(git.getRepository().getDirectory().getParentFile());
             System.out.println("TEST FINNISHED, all tests have been run");
 
             // 4th notify GitHub test results
             // success = all test passed, failure = one or more tests failed
-            // TODO: fix check to see if test passed or not
-            String testState = "";
-            createCommitStatus(repositoryName, commitSHA, "success", token);
+            String testGitState = "";
+            String testGitDescription = "";
+            // count amount of tests that failed, if -1 no errors were found
+            int testFailCount = getTestFailures(testOutput);
+            System.out.println("Number of tests not passed: " + testFailCount);
+            if (testFailCount <= 0) {
+                testGitState = "success";
+                testGitDescription = "all CI tests passed";
+                // set test state to true, all test passed
+                testState = true;
+            } else {
+                testGitState = "failure";
+                testGitDescription = "[" + testFailCount + "] unit tests failed";
+            }
+            // report back the test results as Git status
+            createCommitStatus(repositoryFullName, commitSHA, testGitState, token, testGitDescription);
 
         } catch (GitAPIException e) {
             System.err.println("[ERROR] FAILED to CLONE repository...");
             e.printStackTrace();
         }
         catch (IOException e) {
-            createCommitStatus(repositoryName, commitSHA, "error", token);
+            createCommitStatus(repositoryFullName, commitSHA, "error", token, "compile failed");
+            compileState = false;
             e.printStackTrace();
         }
         catch (InterruptedException e) {
-            createCommitStatus(repositoryName, commitSHA, "error", token);
+            createCommitStatus(repositoryFullName, commitSHA, "error", token, "CI server was interrupted");
             System.err.println("[ABORT] repository compile INTERRUPTED.");
+            compileState = false;
             e.printStackTrace();
         }
         // if this error is reached, we failed to create a commit status, so do not try to do it again
@@ -259,6 +299,15 @@ public class App extends AbstractHandler {
 
             boolean testOutputState = testOutput != "";
             System.out.println("READ test data: " + testOutputState);
+            
+            // save the data
+            // extract branch name
+            String[] parts = branch.split("heads/");
+            String branchName = parts[1];
+            // lastly save all data to a json-file
+            saveData(repositoryName, branchName, commitName, commitSHA, commitTimestamp, compileState, testState, 
+                compileOutput, testOutput, commitURL);
+
             System.out.println("CI-actions finnished");
         }
     }
@@ -325,7 +374,7 @@ public class App extends AbstractHandler {
      * Java.
      * Compiles the repository and catches any errors.
      * 
-     * @param repoDir
+     * @param repoDir location of the repository directory locally on the server
      * @throws IOException
      * @throws InterruptedException
      */
@@ -353,7 +402,19 @@ public class App extends AbstractHandler {
         return output;
     }
 
-    public static void createCommitStatus(String repoName, String commitSHA, String state, String token) {
+    /**
+     * Function to create and update commit status on GitHub. Used for notification.
+     * The function establishes a connection to the GitHub API, and builds
+     * a POST request with information regarding the status of the commit on the CI server.
+     * 
+     * @param repoName the repositorys FULL name
+     * @param commitSHA the commit hash
+     * @param state the state of the commit, can be in four states see GitHub for more info.
+     * @param token secret access token to GitHub repository
+     * @param description additional information regarding the commit status, displayed on GitHub
+     */
+    public static void createCommitStatus(String repoName, String commitSHA, String state, String token,
+        String description) {
         
         try {
             // define URL to GitHub API
@@ -373,9 +434,9 @@ public class App extends AbstractHandler {
             // create the JSON content with the state
             JSONObject contentJSON = new JSONObject();
             contentJSON.put("state", state);
-            contentJSON.put("context", "ci");
-            contentJSON.put("description", "CI Status");
-            contentJSON.put("target_url", "https://www.google.se/?hl=sv");
+            contentJSON.put("context", "CI server");
+            contentJSON.put("description", description);
+            contentJSON.put("target_url", CI_FRONTEND_URL);
             
             String JSONString = contentJSON.toJSONString();
 
@@ -404,7 +465,13 @@ public class App extends AbstractHandler {
         }
     }
 
-    // TODO: add documentation
+    /**
+     * Reads the GitHub access token from a secret file.
+     * 
+     * @param filePath the file path for the secret token, should be in a text file under secret folder
+     * @return the GitHub token as a string
+     * @throws IOException
+     */
     public static String readToken(String filePath)
             throws IOException {
 
@@ -428,6 +495,29 @@ public class App extends AbstractHandler {
         }
     }
 
+    /**
+     * Extracts the number of failed tests from mvn test output
+     *
+     * @param testOutput The output from the mvn test function
+     * @return The number of failed tests from the testoutput or -1
+     * if no match is found or an error occurs during parsing.
+     * */
+    public int getTestFailures(String testOutput) {
+        String regex = "Tests run: \\d+, Failures: \\d+, Errors: (\\d+), Skipped: \\d+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(testOutput);
+
+        while (matcher.find()) {
+            try {
+                return Integer.parseInt(matcher.group(1));
+            } catch (NumberFormatException e) {
+                // do nothing
+            }
+        }
+
+        return -1;
+    }
+
     // TODO: add documentation
     private StringBuilder readRequest(Request baseRequest) {
         try {
@@ -449,6 +539,60 @@ public class App extends AbstractHandler {
             System.err.println("[ERROR] FAILED to read request.");
             e.printStackTrace();
             return null;
+        }
+    }
+
+    /**
+     * Function that saves the console ouput of compile and test of the CI server
+     * into a JSON file. Used for the front-end website to view CI server history.
+     * File is saved under ci-server > data
+     * 
+     * @param repoName name of repository
+     * @param branch name of branch
+     * @param commit the commit message (title)
+     * @param commitSHA commit hash, unique
+     * @param time time of commit
+     * @param compilePass if commit passed compile on CI server
+     * @param testPass if commit passed CI server unit tests
+     * @param logCompile console results from compile
+     * @param logTest console results from tests
+     * @param commitURL the git URL for the commit
+     */
+    private void saveData(String repoName, String branch, String commit, String commitSHA, String time,
+        boolean compilePass, boolean testPass, String logCompile, String logTest, String commitURL) {
+        
+        // create directory structure if it does not exist
+        File dir = new File("data/" + repoName + "/" + branch);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        // count the number of files in the directory
+        int commitNumber = dir.listFiles().length + 1;
+
+        JSONObject commitObj = new JSONObject();
+        commitObj.put("number", commitNumber);      // what order the commit is in 
+        commitObj.put("repo", repoName);            // name of repository
+        commitObj.put("branch", branch);            // name of branch
+        commitObj.put("commit", commitSHA);         // commit hash, unique
+        commitObj.put("commit_message", commit);    // the commit message (title)
+        commitObj.put("time", time);                // time of commit
+        commitObj.put("compilePass", compilePass);  // if commit passed compile on CI server
+        commitObj.put("testPass", testPass);        // if commit passed CI server unit tests
+        commitObj.put("log_compile", logCompile);   // console results from compile
+        commitObj.put("log_test", logTest);         // console results from tests
+        commitObj.put("commitURL", commitURL);      // the git URL for the commit
+
+        // set file name as combination of commit order and commit SHA
+        // the largest number is the latest commit, 
+        // commit SHA ensures an unique name ALWAYS
+        String fileName = "#" + commitNumber + " - " + commitSHA;
+        // write JSON object to file
+        try (FileWriter file = new FileWriter(dir.getPath() + "/" + fileName + ".json")) {
+            file.write(commitObj.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
